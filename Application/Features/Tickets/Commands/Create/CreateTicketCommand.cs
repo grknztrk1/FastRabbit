c5aff42464640da7029bf62d0bb226328240df96
Gürkan Öztürk <grkn-ztrk@hotmail.com>
2023-10-08 21:58:17 +0200

    Create

---
+++ Application/Features/Tickets/Commands/Create/CreateTicketCommand.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Tickets.Commands.Create;
+
+public class CreateTicketCommand : IRequest<CreatedTicketResponse>
+{
+    public string Name { get; set; }
+
+
+
+    public class CreateTicketCommandHandler : IRequestHandler<CreateTicketCommand, CreatedTicketResponse>
+    {
+        public Task<CreatedTicketResponse>? Handle(CreateTicketCommand request, CancellationToken cancellationToken)
+        {
+            CreatedTicketResponse createdTicketResponse = new CreatedTicketResponse();
+            createdTicketResponse.Name = request.Name;
+            createdTicketResponse.Id = new Guid();
+
+            return null;
+        }
+    }
+
+}
---
+++ Application/Features/Tickets/Commands/Create/CreatedTicketResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Tickets.Commands.Create;
+
+public class CreatedTicketResponse
+{
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
